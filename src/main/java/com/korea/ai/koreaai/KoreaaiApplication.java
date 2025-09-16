ea54d89b3d02427cec6cfce5e02b3dc591d7af3b
bswan0113 <116549027+bswan0113@users.noreply.github.com>
2025-09-16 18:04:48 +0900

    initial commit

---
+++ src/main/java/com/korea/ai/koreaai/KoreaaiApplication.java
@@ -0,0 +1,13 @@
+package com.korea.ai.koreaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreaaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreaaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korea/ai/koreaai/KoreaaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korea.ai.koreaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreaaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
